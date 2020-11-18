cf3cd44e47513f2e1ee7760bbc9120b77f749a11
4ndr5 <70884475+4ndr5@users.noreply.github.com>
2020-11-18 14:07:05 +0200

    Connected to db

--- application.py
+++ application.py
@@ -1,18 +1,35 @@
 from flask import Flask, render_template
 from wtform_fields import *
+from models import *
 
-
-
-from wtform_fields import *
+# App configuartion
 app = Flask(__name__)
 app.secret_key = 'csrf.token'
+
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://ztgdtarcblhscz:f75e1a6cd2bcfd802b30e756567c9e448190fa5dd1a51c2670a3d6919fc379ac@ec2-54-224-175-142.compute-1.amazonaws.com:5432/d8h1aim9ltuv4k' 
+
+db = SQLAlchemy(app)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
 
     reg_form = RegistrationForm()
+
     if reg_form.validate_on_submit():
-        return "Success"
+        username = reg_form.username.data
+        password = reg_form.password.data
+
+        # Checking if a user exists
+        user_object = User.query.filter_by(username=username).first()
+        if user_object:
+            return "Username already taken"
+
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return "Insterted into a db!"
+
     return render_template("index.html", form=reg_form)
 
 
