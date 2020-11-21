6c72285a785fb9a97229bcfca5d222bbe16e7421
4ndr5 <70884475+4ndr5@users.noreply.github.com>
2020-11-21 14:47:34 +0200

    connection line change

--- static/scripts/socketio.js
+++ static/scripts/socketio.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var socket = io.connect('http://' + document.domain + ':' + location.port);
+    // var socket = io.connect('http://' + document.domain + ':' + location.port);
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
     let room = "Lounge";
     joinRoom("Lounge");   
 
