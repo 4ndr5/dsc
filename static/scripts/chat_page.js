3079650d11bee94871c49ae7cd3f7466693cc310
4ndr5 <70884475+4ndr5@users.noreply.github.com>
2020-11-23 14:47:27 +0200

    colors

--- static/scripts/chat_page.js
+++ static/scripts/chat_page.js
@@ -1,10 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-       // Destroy the living hell out of sidebar
-       document.querySelector('#show-sidebar-button').onclick = () => {
-        document.querySelector('#sidebar').classList.toggle('view-sidebar');
-    };
-    
     //make 'enter' key submit msg
     let msg = document.querySelector('#user_message');
     msg.addEventListener('keyup', event => {
